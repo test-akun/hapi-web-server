2d251fcdc068356e14aa46bcb610c2e3477a7b11
Ardato <codedokter@gmail.com>
2022-11-20 14:20:59 +0700

    part2 - routing

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 "use strict";
 
 const hapi = require("@hapi/hapi");
+const routes = require("./routes");
 
 const init = async () => {
   const server = hapi.server({
     port: 4000,
     host: "localhost",
   });
+
+  server.route(routes);
 
   await server.start();
   console.log(`Server berjalan pada ${server.info.uri}`);
